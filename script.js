e66d5ddb128ef77f44f325edf5807ceddb8b1beb
Yuriy <dahakayuriy@gmail.com>
2021-12-16 15:10:07 +0200

    FirstCommit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let score = 0;
+let answer = '';
+answer = prompt ('Зимой и летом одним цветом?');
+if (answer === 'Ёлка'){
+  alert ('Yes!');
+  score = score + 1;
+} else {
+  alert ('No(!');
+}
+
+answer = prompt ('Кто его раздевает, тот слёзы пролевает');
+if (answer === 'Лук'){
+  alert ('Yes!');
+  score = score + 1;
+} else {
+  alert ('No(!');
+}
+
+alert ('Верных ответов : ' + score);
+
+
