ee6713c2369938ea87252b89d4bec8bdc7ace628
Vadym Baryshnikov <vadim.barishnikoff@gmail.com>
2021-12-23 12:52:09 +0200

    test git

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ let answer = '';
 answer = prompt ('Зимой и летом одним цветом?');
 if (answer === 'Ёлка'){
   alert ('Yes!');
+  console.log('test');
   score = score + 1;
 } else {
   alert ('No(!');
+  console.log('test');
 }
 
 answer = prompt ('Кто его раздевает, тот слёзы пролевает');
 if (answer === 'Лук'){
   alert ('Yes!');
+  console.log('test');
   score = score + 1;
 } else {
   alert ('No(!');
+  console.log('test');
 }
 
 alert ('Верных ответов : ' + score);
+console.log('test');
 console.log ("Hi github");
 
